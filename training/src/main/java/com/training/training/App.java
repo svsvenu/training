bdde78016ce7a8bb39f9dc47e50867f442561924
venu <svsvenu@gmailcom>
2014-05-24 16:52:41 -0500

    commit 4

--- training/src/main/java/com/training/training/App.java
+++ training/src/main/java/com/training/training/App.java
@@ -6,12 +6,12 @@ package com.training.training;
  */
 public class App 
 
-//I am happy with my current code commit 3
+//I am happy with my current code commit 4
 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! commit3" );
+        System.out.println( "Hello World! commit4" );
 
     }
 }
