512a2bf4f53858b236a9cc4041f266d0a898020f
venu <svsvenu@gmailcom>
2014-05-31 13:54:40 -0500

    Added MathUtil and MathUtil Impl

---
+++ training/src/main/java/com/training/training/MathUtil.java
@@ -0,0 +1,10 @@
+package com.training.training;
+
+public interface MathUtil {
+	
+	    public void add(Integer a, Integer b ) throws Exception;
+	       public void subtract (Integer a, Integer b ) throws Exception;
+	       public void divide (Integer a, Integer b ) throws Exception;
+	       public void multiply ( Integer a, Integer b ) throws Exception;
+
+}
---
+++ training/src/main/java/com/training/training/MathUtilImpl.java
@@ -0,0 +1,27 @@
+package com.training.training;
+
+public class MathUtilImpl implements MathUtil {
+
+	public void add(Integer a, Integer b) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+	public void subtract(Integer a, Integer b) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void divide(Integer a, Integer b) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void multiply(Integer a, Integer b) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
